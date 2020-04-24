a61a490a10c0a48e9b52b3fca2f64f613ce9bef2
Nick Colley <2445413+nickcolley@users.noreply.github.com>
2020-04-24 17:17:39 +0100

    Add sortNested filter
    Refactor code into filters file to keep the entrypoint less cluttered

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,19 @@
+const { groupByNested, sortNested } = require('./lib/filters.js')
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets")
     eleventyConfig.addPassthroughCopy("src/favicon.ico")
     eleventyConfig.addPassthroughCopy("src/**/*.png")
     eleventyConfig.addPassthroughCopy("src/**/*.jpg")
 
-    // From https://github.com/mozilla/nunjucks/issues/1198#issuecomment-610494129
-    eleventyConfig.addNunjucksFilter("groupByNested", function (arr, key) {
-        const result = {};
-        arr.forEach(item => {
-            const keys = key.split('.');
-            const value = keys.reduce((object, key) => object[key], item);
-    
-            (result[value] || (result[value] = [])).push(item);
-        });
-        return result;
-    })
+    function getNestedPropertyValue (selector, originalObject) {
+        let keys = selector.split('.');
+        let value = keys.reduce((object, key) => object[key], originalObject);
+        return value;
+    }
+
+    eleventyConfig.addNunjucksFilter("groupByNested", groupByNested)
+    eleventyConfig.addNunjucksFilter("sortNested", sortNested)
 
     return {
         dir: {
