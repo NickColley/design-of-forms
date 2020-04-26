4de4e25c75c465bff405ade34fe760a1d27d9031
Nick Colley <2445413+nickcolley@users.noreply.github.com>
2020-04-26 22:38:04 +0100

    Remove unused function

--- .eleventy.js
+++ .eleventy.js
@@ -6,12 +6,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/**/*.png")
     eleventyConfig.addPassthroughCopy("src/**/*.jpg")
 
-    function getNestedPropertyValue (selector, originalObject) {
-        let keys = selector.split('.');
-        let value = keys.reduce((object, key) => object[key], originalObject);
-        return value;
-    }
-
     eleventyConfig.addNunjucksFilter("groupByNested", groupByNested)
     eleventyConfig.addNunjucksFilter("sortNested", sortNested)
 
