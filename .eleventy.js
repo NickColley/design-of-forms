18b35c4d0d5ca226cbf890f71ef29bdd746c73e7
Nick Colley <2445413+nickcolley@users.noreply.github.com>
2020-08-16 22:53:02 +0100

    Refactor transformations

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 const fg = require('fast-glob');
-const cheerio = require('cheerio');
 
 const { groupByNested, sortNested } = require('./lib/filters.js')
-const markdown = require('./lib/markdown')
+const markdown = require('./lib/markdown');
+const semanticLists = require('./lib/transforms/semantic-lists.js');
+const semanticFigures = require('./lib/transforms/semantic-figures.js');
 
 const galleryImages = fg.sync(['**/*.jpg', '!**/_site']);
 
@@ -17,35 +18,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addNunjucksFilter("sortNested", sortNested)
 
     eleventyConfig.setLibrary("md", markdown)
+    eleventyConfig.addTransform("semantic-lists", semanticLists);
+    eleventyConfig.addTransform("semantic-figures", semanticFigures);
+
     eleventyConfig.addCollection('gallery', () => galleryImages.map(image => image.replace('src/', '/')))
-
-    // Make sure styled lists keep their correct semantics.
-    // https://www.scottohara.me/blog/2019/01/12/lists-and-safari.html
-    eleventyConfig.addTransform("semantic-lists", async function(content, outputPath) {
-        if(outputPath.endsWith(".html")) {
-            const $ = cheerio.load(content);
-            $('ol,ul').attr('role', 'list');
-            $('li').attr('role', 'listitem');
-            content = $.html();
-        }
-        return content;
-    });
-
-    // Make figures work better
-    // https://www.scottohara.me/blog/2019/01/21/how-do-you-figure.html
-    eleventyConfig.addTransform("semantic-figure", async function(content, outputPath) {
-        if(outputPath.endsWith(".html")) {
-            const $ = cheerio.load(content);
-            $('figure').each(function () {
-                const $figure = $(this);
-                const $caption = $figure.find('figcaption');
-                $figure.attr('role', 'figure');
-                $figure.attr('aria-label', $caption.text().trim());
-            })
-            content = $.html();
-        }
-        return content;
-    });
 
     return {
         dir: {
