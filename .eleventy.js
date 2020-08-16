3d6b5f08f4e20ee2d991cb59b184245ccddb882c
Nick Colley <2445413+nickcolley@users.noreply.github.com>
2020-08-16 23:27:47 +0100

    Add transform for counter variables
    So dont need sorta hacky hardcoded ones

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const { groupByNested, sortNested } = require('./lib/filters.js')
 const markdown = require('./lib/markdown');
 const semanticLists = require('./lib/transforms/semantic-lists.js');
 const semanticFigures = require('./lib/transforms/semantic-figures.js');
+const counterStartLists = require('./lib/transforms/counter-start-lists.js');
 
 const galleryImages = fg.sync(['**/*.jpg', '!**/_site']);
 
@@ -20,6 +21,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setLibrary("md", markdown)
     eleventyConfig.addTransform("semantic-lists", semanticLists);
     eleventyConfig.addTransform("semantic-figures", semanticFigures);
+    eleventyConfig.addTransform("counter-start-lists", counterStartLists);
 
     eleventyConfig.addCollection('gallery', () => galleryImages.map(image => image.replace('src/', '/')))
 
