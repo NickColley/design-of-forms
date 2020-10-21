f0177108c4931cb70e55edc2cced67c106eae596
Nick Colley <2445413+nickcolley@users.noreply.github.com>
2020-10-21 19:40:34 +0100

    Add theme switcher

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -1,1 +1,17 @@
-console.log('Hello, world.')
+function setTheme(name) {
+  document.documentElement.dataset.theme = name;
+  window.localStorage.setItem("theme", name);
+}
+
+const $themeSelect = document.querySelector("[data-theme-select]");
+// JavaScript is avaliable so show the select.
+$themeSelect.removeAttribute("hidden");
+$themeSelect.addEventListener("input", (event) => setTheme(event.target.value));
+
+const $defaultCheckedInput = document.querySelector("[name=theme][checked]");
+const defaultTheme =
+  window.localStorage.getItem("theme") || $defaultCheckedInput.value;
+
+$defaultCheckedInput.checked = false;
+$themeSelect.querySelector(`[name=theme][value="${defaultTheme}"]`).checked = true;
+setTheme(defaultTheme);
