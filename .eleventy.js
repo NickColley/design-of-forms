452b72208d41e6d6e43f54990ad3a1cd2ef61a2a
Nick Colley <2445413+nickcolley@users.noreply.github.com>
2020-10-28 21:45:07 +0000

    Lint files

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,35 @@
-const { groupByNested } = require('./lib/filters.js')
-const markdown = require('./lib/markdown');
-const semanticLists = require('./lib/transforms/semantic-lists.js');
-const counterStartLists = require('./lib/transforms/counter-start-lists.js');
-const sortedPages = require('./lib/collections/sorted-pages.js')
-const gallery = require('./lib/collections/gallery.js')
+const { groupByNested } = require("./lib/filters.js");
+const markdown = require("./lib/markdown");
+const semanticLists = require("./lib/transforms/semantic-lists.js");
+const counterStartLists = require("./lib/transforms/counter-start-lists.js");
+const sortedPages = require("./lib/collections/sorted-pages.js");
+const gallery = require("./lib/collections/gallery.js");
 const accessibilityPlugin = require("eleventy-plugin-accessibility");
 
-module.exports = function(eleventyConfig) {
-    if (process.env.NODE_ENV === 'test') {
-        eleventyConfig.addPlugin(accessibilityPlugin);
-    }
+module.exports = function (eleventyConfig) {
+  if (process.env.NODE_ENV === "test") {
+    eleventyConfig.addPlugin(accessibilityPlugin);
+  }
 
-    eleventyConfig.addPassthroughCopy("src/assets")
-    eleventyConfig.addPassthroughCopy("src/favicon.ico")
-    eleventyConfig.addPassthroughCopy("src/**/*.png")
-    eleventyConfig.addPassthroughCopy("src/**/*.jpg")
-    eleventyConfig.addPassthroughCopy("src/**/*.webp")
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/**/*.png");
+  eleventyConfig.addPassthroughCopy("src/**/*.jpg");
+  eleventyConfig.addPassthroughCopy("src/**/*.webp");
 
-    eleventyConfig.addNunjucksFilter("groupByNested", groupByNested)
+  eleventyConfig.addNunjucksFilter("groupByNested", groupByNested);
 
-    eleventyConfig.setLibrary("md", markdown)
-    eleventyConfig.addTransform("semantic-lists", semanticLists);
-    eleventyConfig.addTransform("counter-start-lists", counterStartLists);
+  eleventyConfig.setLibrary("md", markdown);
+  eleventyConfig.addTransform("semantic-lists", semanticLists);
+  eleventyConfig.addTransform("counter-start-lists", counterStartLists);
 
-    // Sort pages based on order in the original book.
-    eleventyConfig.addCollection("sortedPages", sortedPages);
-    eleventyConfig.addCollection('gallery', gallery(markdown));
+  // Sort pages based on order in the original book.
+  eleventyConfig.addCollection("sortedPages", sortedPages);
+  eleventyConfig.addCollection("gallery", gallery(markdown));
 
-    return {
-        dir: {
-            input: "src"
-        }
-    }
-}
+  return {
+    dir: {
+      input: "src",
+    },
+  };
+};
