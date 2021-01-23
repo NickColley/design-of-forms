22e484627812c6ba8a967cb9b1324cbc723d096b
Nick Colley <2445413+nickcolley@users.noreply.github.com>
2021-01-23 22:02:59 +0000

    Feature detect showing theme changer
    Make it slightly nicer in IE11

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -1,26 +1,40 @@
-function getCSSCustomPropertyValue(name) {
-  return window
-    .getComputedStyle(document.documentElement)
-    .getPropertyValue("--" + name)
-    .trim();
+function main() {
+  const supportsCSSCustomProperties =
+    window.CSS && CSS.supports("color", "var(--property)");
+
+  if (!supportsCSSCustomProperties) {
+    return;
+  }
+
+  const $themeSelect = document.querySelector("[data-theme-select]");
+  $themeSelect.removeAttribute("hidden");
+  $themeSelect.addEventListener("input", (event) =>
+    setTheme(event.target.value)
+  );
+
+  const $defaultCheckedInput = document.querySelector("[name=theme][checked]");
+  const defaultTheme =
+    document.documentElement.dataset.theme || $defaultCheckedInput.value;
+
+  $defaultCheckedInput.checked = false;
+  $themeSelect.querySelector(
+    `[name=theme][value="${defaultTheme}"]`
+  ).checked = true;
+  setTheme(defaultTheme);
+
+  function getCSSCustomPropertyValue(name) {
+    return window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue("--" + name)
+      .trim();
+  }
+
+  function setTheme(name) {
+    document.documentElement.dataset.theme = name;
+    const $metaThemeColor = document.querySelector('meta[name="theme-color"]');
+    $metaThemeColor.content = getCSSCustomPropertyValue("background-color");
+    window.localStorage.setItem("theme", name);
+  }
 }
 
-function setTheme(name) {
-  document.documentElement.dataset.theme = name;
-  $metaThemeColor.content = getCSSCustomPropertyValue("background-color");
-  window.localStorage.setItem("theme", name);
-}
-
-const $metaThemeColor = document.querySelector('meta[name="theme-color"]');
-const $themeSelect = document.querySelector("[data-theme-select]");
-$themeSelect.addEventListener("input", (event) => setTheme(event.target.value));
-
-const $defaultCheckedInput = document.querySelector("[name=theme][checked]");
-const defaultTheme =
-  document.documentElement.dataset.theme || $defaultCheckedInput.value;
-
-$defaultCheckedInput.checked = false;
-$themeSelect.querySelector(
-  `[name=theme][value="${defaultTheme}"]`
-).checked = true;
-setTheme(defaultTheme);
+main();
